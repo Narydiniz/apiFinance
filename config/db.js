9b0bab03ff29c81d93201d02e57eec9554747711
Narydiniz <senainaryara@gmail.com>
2024-07-24 10:44:37 -0300

    Conectando ao banco de dados

---
+++ config/db.js
@@ -0,0 +1,19 @@
+//Importar a iblioteca mysql2 e criar a conexão  com o banco de dados
+const mysql= require('mysql2'); // importa o pacote mysql2 para conectar
+const db = mysql.createConnection({
+    host:process.env.DB_HOST, // Endereço do servidor de banco de dados
+    user:process.env.DB_USER, // Nome do Usuario para acessar o banco de dados
+    password:process.env.DB_PASS, // Senha do usuario  para acessar o Banco de dados
+    database:process.env.DB_NAME // Nome do banco de dados
+});
+
+db.connect((err) => {
+    if(err) {
+        console.log('Erro ao conectar ao Banco de Dados.', err); // Exibe a mensagem de Erro
+    return;
+    }
+
+    console.log('Conectando ao Banco de Dados Mysql'); // Exibe mensagem de Sucesso 
+});
+
+module.exports=db; // Exporta a conexão para ser usada em outros arquivos 
--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const app = express();                    //Inicializa uma nova aplicação
 
 
 // configurar o Cors e o bodyParse
-app.use(cors())                           //habilita o cors para todas as rotas
+app.use(cors());                           //habilita o cors para todas as rotas
 app.use(bodyParser.json());                // configura o bodyParse para analisar requisições Json;
 
 // Rota inicial para testar o servidor
 app.get('/', (req, res) => {
-    res.send('Servidor está rodando')     //definir uma rota para testar o servidor
+    res.send('Servidor está rodando');    //definir uma rota para testar o servidor
 });
 
 
@@ -27,5 +27,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;    //define a porta a partir da variavel de ambiente ou usa a porta 3000 por padrão
 app.listen(PORT,()  => {
-    console.log(`Servidor rodando na porta ${PORT}`)     //envia uma mensagem informando que o servidor esta rodand na ${PORT}
-})
+    console.log(`Servidor rodando na porta ${PORT}`);    //envia uma mensagem informando que o servidor esta rodand na ${PORT}
+});
